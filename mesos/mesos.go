7db8f85a7c1af9d9daa2af6b301e7a209fab93d9
Dominic Hamon <dhamon@twitter.com>
2014-07-30 13:39:01 -0700

    Remove type.go and move types to where they're used

--- mesos/mesos.go
+++ mesos/mesos.go
@@ -9,6 +9,32 @@ import (
 	"github.com/twitter/gozer/proto/scheduler.pb"
 )
 
+type MasterAddress struct {
+	Hostname string
+	Port     int
+}
+
+type DriverConfig struct {
+	FrameworkName  string
+	RegisteredUser string
+	Masters        []MasterAddress
+}
+
+type Driver struct {
+	config    DriverConfig
+	pidIp     string
+	pidPort   int
+
+	frameworkId mesos.FrameworkID
+
+	command chan func(*Driver) error
+	// TODO(weingart): move to internal type to handle master disconnect, error events/etc.
+	events chan *mesos_scheduler.Event
+
+	Offers  chan *mesos.Offer
+	Updates chan *TaskStateUpdate
+}
+
 func newDriver(mc *DriverConfig) (d *Driver, err error) {
 	name, err := os.Hostname()
 	if err != nil {
