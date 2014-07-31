bb49895e476c574dc306269bffe41f4ae28845e8
Dominic Hamon <dhamon@twitter.com>
2014-07-31 10:29:39 -0700

    Add new logging style logs

--- gozer/http.go
+++ gozer/http.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -11,9 +10,9 @@ var taskIndex = 0
 
 func startHTTP() {
 	http.HandleFunc("/api/addtask", addTaskHandler)
-	log.Printf("api listening on port %d", *port)
+	log.Info.Printf("API listening on port %d", *port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
-		log.Fatalf("failed to start listening on port %d", *port)
+		log.Error.Fatalf("Failed to start listening on port %d", *port)
 	}
 }
 
@@ -21,14 +20,14 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Add("Allow", "POST")
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		log.Printf("received addtask request with unexpected method. want %q, got %q: %+v", "POST", r.Method, r)
+		log.Error.Printf("Received addtask request with unexpected method. want %q, got %q: %+v", "POST", r.Method, r)
 	}
 	defer r.Body.Close()
 
 	var task Task
 	err := json.NewDecoder(r.Body).Decode(&task)
 	if err != nil {
-		log.Printf("ERROR: failed to parse JSON body from addtask request %+v: %+v", r, err)
+		log.Error.Printf("Failed to parse JSON body from addtask request %+v: %+v", r, err)
 		// TODO(dhamon): Better error for this case.
 		w.WriteHeader(http.StatusInternalServerError)
 		return
